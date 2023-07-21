ad3398b4a9b16e1df4f6d467200e219005c2904e
ziangit <ziangs@andrew.cmu.edu>
2023-07-21 11:31:06 -0500

    v1.0

---
+++ src/main/java/com/zs/productservice/MicroserviceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.zs.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceProjectApplication.class, args);
+    }
+
+}
